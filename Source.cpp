a5d23ca53af011b5352a841c84219cf4ea18cd3f
ytw060309 <65099447+ytw060309@users.noreply.github.com>
2022-02-20 13:20:53 +0900

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Editor.h"
+using namespace std;
+int main() {
+	Editor editor;
+	cout << "[ADOFAI All Simultaneously Hit Convertor]" << endl << endl;
+	cout << "맵의 모든 타일을 15도 동타 타일로 변경합니다" << endl;
+	cout << "Developer : CLiF" << endl << endl;
+	cout << "Version : 1.0" << endl << endl;
+
+	cout << "ADOFAI File Path : ";
+	string path;
+	cin >> path;
+	cout << "Converting . . ." << endl;
+	editor.Open(path);
+	editor.ChangeFile();
+	cout << "Saving . . ." << endl;
+	editor.Save(path);
+	cout << "Completed!!" << endl;
+	system("pause");
+	return 0;
+}
